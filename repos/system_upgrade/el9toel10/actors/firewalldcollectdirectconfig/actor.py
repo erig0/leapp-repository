7cd5325ccae7ca058e42e5c33b2e7b12c11dc830
Eric Garver <egarver@redhat.com>
2024-10-22 17:50:37 -0400

    actors: add FirewalldCollectDirectConfig

---
+++ repos/system_upgrade/el9toel10/actors/firewalldcollectdirectconfig/actor.py
@@ -0,0 +1,15 @@
+from leapp.actors import Actor
+
+
+class FirewalldCollectDirectConfig(Actor):
+    """
+    No documentation has been provided for the firewalld_collect_direct_config actor.
+    """
+
+    name = 'firewalld_collect_direct_config'
+    consumes = ()
+    produces = ()
+    tags = ()
+
+    def process(self):
+        pass
